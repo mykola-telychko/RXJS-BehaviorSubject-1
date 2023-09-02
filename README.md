ac046015b893149056afaf2a22b83ed08c63eb1d
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-09-02 20:00:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-BehaviorSubject-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-behaviorsubject-simpleexample-hualg8)
---
+++ index.ts
@@ -0,0 +1,22 @@
+console.clear();
+import { BehaviorSubject } from 'rxjs';
+
+// https://www.learnrxjs.io/learn-rxjs/subjects/behaviorsubject
+// Example 1: Simple BehaviorSubject
+
+const subject = new BehaviorSubject(123);
+
+//two new subscribers will get initial value => output: 123, 123
+subject.subscribe(console.log);
+subject.subscribe(console.log);
+
+//two subscribers will get new value => output: 456, 456
+subject.next(456);
+
+//new subscriber will get latest value (456) => output: 456
+subject.subscribe(console.log);
+
+//all three subscribers will get new value => output: 789, 789, 789
+subject.next(789);
+
+// output: 123, 123, 456, 456, 456, 789, 789, 789
